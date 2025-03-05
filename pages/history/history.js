6a7f131616907681b83eeeed99751166532f891d
Xiaoyang Liu <xiaoyangliu@microsoft.com>
2025-03-05 17:09:05 +0800

    feat: modify styles

--- pages/history/history.js
+++ pages/history/history.js
@@ -10,5 +10,14 @@ Page({
     this.setData({
       historyList: history
     })
+  },
+  formatTime: function(timestamp) {
+    const date = new Date(timestamp)
+    const year = date.getFullYear()
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
+    const day = date.getDate().toString().padStart(2, '0')
+    const hour = date.getHours().toString().padStart(2, '0')
+    const minute = date.getMinutes().toString().padStart(2, '0')
+    return `${year}-${month}-${day} ${hour}:${minute}`
   }
 }) 
